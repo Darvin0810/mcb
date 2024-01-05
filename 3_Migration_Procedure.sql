8ab38da2fc29fe2b87ff8c4275c8e282f710ac56
Darvin0810 <darvinhurruck@gmail.com>
2024-01-05 14:55:43 +0400

    Correction of data issue to handle missed typed values in number and dates

--- 3_Migration_Procedure.sql
+++ 3_Migration_Procedure.sql
@@ -16,6 +16,9 @@ END OrderMigrationPackage;
 CREATE OR REPLACE PACKAGE BODY OrderMigrationPackage AS
   -- Procedure to migrate data from XXBCM_ORDER_MGT to normalized tables
   PROCEDURE MigrateData IS
+   v_date_str VARCHAR2(20);
+   v_date     DATE;
+   v_formatted_date VARCHAR2(20);
   BEGIN
     FOR order_rec IN (SELECT * FROM XXBCM_ORDER_MGT) LOOP
       -- Insert data into Suppliers table
@@ -35,6 +38,18 @@ CREATE OR REPLACE PACKAGE BODY OrderMigrationPackage AS
         order_rec.SUPP_EMAIL
       );
 
+      -- Start Handling date issue
+       v_date_str := order_rec.ORDER_DATE;
+      BEGIN
+         v_date := TO_DATE(v_date_str, 'DD-MON-YYYY');
+      EXCEPTION
+         WHEN OTHERS THEN
+            v_date := TO_DATE(v_date_str, 'DD-MM-YYYY');
+      END;
+      v_formatted_date := TO_CHAR(v_date, 'DD-MON-YYYY');
+      v_date := TO_DATE(v_formatted_date, 'DD-MON-YYYY');
+      -- End Handling Date Issue
+
       -- Insert data into Orders table
       INSERT INTO Orders (
         Order_ID,
@@ -47,9 +62,9 @@ CREATE OR REPLACE PACKAGE BODY OrderMigrationPackage AS
       ) VALUES (
         OrderSeq.NEXTVAL,
         order_rec.ORDER_REF,
-        TO_DATE(order_rec.ORDER_DATE, 'DD-MON-YYYY'),  -- Adjust the date format if needed
+        TO_DATE(v_date, 'DD-MON-YYYY'),  -- Adjust the date format if needed
         SupplierSeq.CURRVAL,
-        TO_NUMBER(order_rec.ORDER_TOTAL_AMOUNT),
+        TO_NUMBER(REPLACE(TRANSLATE(NVL(order_rec.ORDER_TOTAL_AMOUNT, '0'), 'OoIiSs-', '0011550'), ',', '')),
         order_rec.ORDER_DESCRIPTION,
         order_rec.ORDER_STATUS
       );
@@ -62,8 +77,20 @@ CREATE OR REPLACE PACKAGE BODY OrderMigrationPackage AS
       ) VALUES (
         OrderLineSeq.NEXTVAL,
         OrderSeq.CURRVAL,
-        TO_NUMBER(order_rec.ORDER_LINE_AMOUNT)
+        TO_NUMBER(REPLACE(TRANSLATE(NVL(order_rec.ORDER_LINE_AMOUNT, '0'), 'OoIiSs-', '0011550'), ',', ''))
       );
+
+       -- Start Handling date issue
+       v_date_str := order_rec.INVOICE_DATE;
+      BEGIN
+         v_date := TO_DATE(v_date_str, 'DD-MON-YYYY');
+      EXCEPTION
+         WHEN OTHERS THEN
+            v_date := TO_DATE(v_date_str, 'DD-MM-YYYY');
+      END;
+      v_formatted_date := TO_CHAR(v_date, 'DD-MON-YYYY');
+      v_date := TO_DATE(v_formatted_date, 'DD-MON-YYYY');
+      -- End Handling Date Issue
 
       -- Insert data into Invoices table
       INSERT INTO Invoices (
@@ -79,10 +106,10 @@ CREATE OR REPLACE PACKAGE BODY OrderMigrationPackage AS
         InvoiceSeq.NEXTVAL,
         OrderSeq.CURRVAL,
         order_rec.INVOICE_REFERENCE,
-        TO_DATE(order_rec.INVOICE_DATE, 'DD-MON-YYYY'),  -- Adjust the date format if needed
+        TO_DATE(v_date, 'DD-MON-YYYY'),  -- Adjust the date format if needed
         order_rec.INVOICE_STATUS,
         order_rec.INVOICE_HOLD_REASON,
-        TO_NUMBER(order_rec.INVOICE_AMOUNT),
+        TO_NUMBER(REPLACE(TRANSLATE(NVL(order_rec.INVOICE_AMOUNT, '0'), 'OoIiSs-', '0011550'), ',', '')),
         order_rec.INVOICE_DESCRIPTION
       );
 
@@ -95,7 +122,7 @@ CREATE OR REPLACE PACKAGE BODY OrderMigrationPackage AS
       ) VALUES (
         PaymentSeq.NEXTVAL,
         InvoiceSeq.CURRVAL,
-        TO_NUMBER(order_rec.INVOICE_AMOUNT),  -- Assuming invoice amount is also the payment amount
+        TO_NUMBER(REPLACE(TRANSLATE(NVL(order_rec.INVOICE_AMOUNT, '0'), 'OoIiSs-', '0011550'), ',', '')),
         SYSDATE  -- Assuming the current date for payment date
       );
 
@@ -103,3 +130,7 @@ CREATE OR REPLACE PACKAGE BODY OrderMigrationPackage AS
   END MigrateData;
 END OrderMigrationPackage;
 /
+
+BEGIN
+  OrderMigrationPackage.MigrateData;
+END;
